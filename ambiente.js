67e11f0f7c8c57e9a5719e29a1b1b38008d62cc1
Mary <maryllianbackup@gmail.com>
2023-07-06 09:40:15 -0300

    desafio 2

--- ambiente.js
+++ ambiente.js
@@ -4,4 +4,11 @@ let nomes = 'Hello, World! OpenAI is amazing.';
 let splitString = nomes.split(' ');
 let reverseArray = splitString.reverse();
 let joinArray = reverseArray.join (' ');
-console.log(joinArray)
+console.log(joinArray);
+
+//Desafio 2
+let frase = 'Hello, World!';
+let set = new Set(frase);
+let fraseSemDuplicados = Array.from(set);
+let joinFrase = fraseSemDuplicados.join(' ');
+console.log(joinFrase);
