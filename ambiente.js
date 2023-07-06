bb367a9b4a1efc8b517eaddbf9acde593b0689f5
Mary <maryllianbackup@gmail.com>
2023-07-06 09:33:20 -0300

    desafio 1

---
+++ ambiente.js
@@ -0,0 +1,7 @@
+// DESAFIO DE MANIPULAÇÃO DE STRINGS
+//Desafio 1
+let nomes = 'Hello, World! OpenAI is amazing.';
+let splitString = nomes.split(' ');
+let reverseArray = splitString.reverse();
+let joinArray = reverseArray.join (' ');
+console.log(joinArray)
